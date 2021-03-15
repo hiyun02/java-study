68636181a83e37eeaff6d5bdaf36ea0f85c8fcd3
hayoon020105 <psalter150@naver.com>
2021-03-15 19:08:04 +0900

    첫 3항연산자(?) 사용

---
+++ problem/src/problem/codeup1063.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Scanner;
+
+public class codeup1063 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s = new Scanner(System.in);
+int a = s.nextInt();
+int b = s.nextInt();
+
+int result = (a>b)? a : b; 
+System.out.println(result);
+	}
+
+}
