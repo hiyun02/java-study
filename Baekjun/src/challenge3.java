13aad3fe678cca59d2626a9de4ad216ea78d2a3f
psalt <psalt@DESKTOP-1IUOD21>
2021-03-23 23:54:49 +0900

    홍기현 선배님이 내주신 백준 구명보트 문제

---
+++ Baekjun/src/challenge3.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class challenge3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int limit = s.nextInt();
+		int n = s.nextInt();
+		int[] people = new int[n];
+		for (int i = 0; i < n; i++)
+			people[i] = s.nextInt();
+		System.out.println(solution(people, limit));
+	}
+
+	public static int solution(int[] people, int limit) {
+
+		int answer = 0;
+
+		Arrays.sort(people);
+
+		for (int i = people.length - 1; i >= 0;) {
+			int sum = limit - people[i];
+
+			for (int j = 0;;) {
+
+				if (sum >= people[j]) {
+					answer++;
+					j++;
+					i--;
+					break;
+				} else {
+					i--;
+					answer++;
+
+				}
+			}
+		}
+
+		return answer;
+	}
+}
