c71339e81ff3a79f10cb6f43bd16df155ec2931f
hayoon020105 <psalter150@naver.com>
2021-03-27 23:06:24 +0900

    베열 원소의 타입

---
+++ problem/src/problem/veq.java
@@ -0,0 +1,11 @@
+package problem;
+
+public class veq {
+	public static void main(String[] args) {
+		//기본형 변수와 배열의 원소
+		String s="hello";
+		String[] arr= {"a","b","c"};
+		System.out.println("String :"+s.getClass().getName());
+		System.out.println("String array :"+arr.getClass().getName());
+	}
+}
