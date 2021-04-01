294b573f601b2074290e39cca342ad370e905fcf
hayoon020105 <psalter150@naver.com>
2021-04-01 23:01:41 +0900

    검문 문제의 시간초과 현상을 피하기 위한 새로운 알고리즘을 생각하기

---
+++ problem/src/baekjoon/challenge6_hint.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class challenge6_hint {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
+		}
+		
+		
+		for (int i = 0; i < n; i++) {
+		
+		}
+
+	}
+
+}
