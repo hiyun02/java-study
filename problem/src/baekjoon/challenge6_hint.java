ab1c3cac1b25248b555bfea00450af7a936d0919
hayoon020105 <psalter150@naver.com>
2021-04-02 20:53:20 +0900

    틀렸지만 수정해서 맞출거임 ;;

--- problem/src/baekjoon/challenge6_hint.java
+++ problem/src/baekjoon/challenge6_hint.java
@@ -1,24 +1,29 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class challenge6_hint {
-
+//나머지의 정의와 수학적 성질을 이용한 알고리즘 만들기
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int[] arr = new int[n];
-		
-
+		int n = s.nextInt(); // 입력받을 숫자의 개수
+		int[] arr = new int[n]; // 입력받은 수를 배열에 저장
 		for (int i = 0; i < n; i++) {
 			arr[i] = s.nextInt();
 		}
+Arrays.sort(arr);
 		
+int a = arr[1]-arr[0];
+int b = arr[2]-arr[0];
 		
-		for (int i = 0; i < n; i++) {
-		
-		}
 
+for(int i=2;i<=a;i++) {
+	if(a%i==0&&b%i==0) {
+		System.out.print(i+" ");
 	}
+}
+		
+			}
 
 }
