ab1fd275cc40be0b0fa2d1ad6f1478d412c9941c
hayoon020105 <psalter150@naver.com>
2021-04-03 12:21:08 +0900

    백준 2588번 나머지와 자릿수 문제

---
+++ Baekjun/src/q2588.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class q2588 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+	
+	System.out.println(a*((b%100)%10));
+	System.out.println(a*((b%100)/10));
+	System.out.println(a*(b/100));
+	System.out.println(a*b);
+	
+	}
+
+}
