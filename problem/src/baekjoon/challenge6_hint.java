492d5c1ef695cfdb9eb8e12c76b53d4eab9645b7
hayoon020105 <psalter150@naver.com>
2021-04-05 19:22:23 +0900

    유클리드 함수를 이용한 가장 짧은 검문 문제 풀이

--- problem/src/baekjoon/challenge6_hint.java
+++ problem/src/baekjoon/challenge6_hint.java
@@ -1,29 +1,48 @@
 package baekjoon;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class challenge6_hint {
-//나머지의 정의와 수학적 성질을 이용한 알고리즘 만들기
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt(); // 입력받을 숫자의 개수
-		int[] arr = new int[n]; // 입력받은 수를 배열에 저장
+		int n = s.nextInt();
+		int[] arr = new int[n];
+
 		for (int i = 0; i < n; i++) {
 			arr[i] = s.nextInt();
 		}
-Arrays.sort(arr);
-		
-int a = arr[1]-arr[0];
-int b = arr[2]-arr[0];
-		
 
-for(int i=2;i<=a;i++) {
-	if(a%i==0&&b%i==0) {
-		System.out.print(i+" ");
-	}
-}
-		
+		Arrays.sort(arr);
+		if (n == 2) {
+			for (int M = 2; M <= arr[1]; M++) {
+				if ((arr[1]-arr[0])%M==0) {
+					System.out.print(M + " ");
+				}
+			}
+		} else {
+			int S = g((arr[2] - arr[0]), (arr[1] - arr[0]));
+
+			for (int i = 3; i < n ; i++) {
+			 
+					S = g((arr[i] - arr[0]), S);
 			}
 
+			int M = 2;
+			while (M <= S) {
+				if (S % M == 0) {
+					System.out.print(M + " ");
+				}
+				M++;
+			}
+		}
+	}
+
+	public static int g(int x, int y) {
+		if (y == 0) {
+			return x;
+		} else {
+			return g(y, x % y);
+		}
+	}
 }
