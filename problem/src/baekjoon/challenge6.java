78b858f8667607939f5d9757296b14c2a0051cbf
hayoon020105 <psalter150@naver.com>
2021-04-05 19:12:10 +0900

    유클리드 함수를 사용하지 않은 검문문제 풀이

--- problem/src/baekjoon/challenge6.java
+++ problem/src/baekjoon/challenge6.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class challenge6 {
 
 	public static void main(String[] args) {
-		//백준 s1 2981번 검문 문제 여러 수의 나머지가 같도록 하는 몫을 모두 찾아라
+		// 백준 s1 2981번 검문 문제 여러 수의 나머지가 같도록 하는 몫을 모두 찾아라
 		// 나의 풀이
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
@@ -15,28 +15,17 @@ public class challenge6 {
 		for (int i = 0; i < n; i++) {
 			arr[i] = s.nextInt();
 		}
-		int[] nam = new int[0];
-		
-		Arrays.sort(arr);
-		
-	
-		
-		
-		slash: for (int i = 2; i < arr[n - 1] - 1; i++) {
 
-			for (int j = 0; j < arr.length; j++) {
-				nam[j] = arr[j] % i;
-			}
-			for (int j = 0; j < nam.length - 1; j++) {
-				if (nam[j] != nam[j + 1]) {
+		Arrays.sort(arr);
+
+		slash : for (int i = 2; i < arr[n - 1]; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] % i != arr[j + 1] % i) {
 					continue slash;
 				}
-
 			}
-			System.out.println(i + " ");
-		 
+			System.out.print(i + " ");
 		}
-		
-	}
 
+	}
 }
