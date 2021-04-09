1fa8e2448ba1b9671a7a5c01be01d70b2a58b097
hayoon020105 <psalter150@naver.com>
2021-04-09 19:03:56 +0900

    백준 11654

---
+++ problem/src/baekjoon/q11654.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q11654 {
+
+	public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+	char c=s.nextLine().charAt(0);
+	System.out.println((int)c);
+	}
+
+}
