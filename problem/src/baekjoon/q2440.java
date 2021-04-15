a27915f660047776e1c5cd350afcf51fd71923e3
hayoon020105 <psalter150@naver.com>
2021-04-15 11:16:56 +0900

    백준 2440 별찍기 3

---
+++ problem/src/baekjoon/q2440.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q2440 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for (int i = n; i >0; i--) {
+			for(int j=0;j<i;j++) {
+				System.out.print('*');
+			}
+		System.out.println();}
+	}
+
+}
