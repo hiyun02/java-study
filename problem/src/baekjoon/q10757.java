085a5250adad18c92818fc66125b73763646a28a
hayoon020105 <psalter150@naver.com>
2021-04-23 22:35:29 +0900

    백준 10757 큰수의 합(long)

---
+++ problem/src/baekjoon/q10757.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q10757 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		long a = s.nextLong();
+		long b = s.nextLong();
+		System.out.println(a+b);
+	}
+
+}
