b4d1b02758fac7b2a2d2f9b04cfa572c87816024
hayoon020105 <psalter150@naver.com>
2021-04-24 03:35:32 +0900

    백준 10757 BigInteger 클래스의 활용

--- problem/src/baekjoon/q10757.java
+++ problem/src/baekjoon/q10757.java
@@ -1,14 +1,18 @@
 package baekjoon;
-
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class q10757 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		long a = s.nextLong();
-		long b = s.nextLong();
-		System.out.println(a+b);
+		String a = s.next();
+		String b = s.next();
+
+			BigInteger bnumber1=new BigInteger(a);
+			BigInteger bnumber2=new BigInteger(b);
+			
+			System.out.println(bnumber1.add(bnumber2));
 	}
 
 }
