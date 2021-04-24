4a2bbf6927db2a45bbf1c46fc6db6aa5a076e07c
hayoon020105 <psalter150@naver.com>
2021-04-25 00:28:04 +0900

    반복문 하나로 구구단 출력해보기

---
+++ problem/src/z/etc/dandan01.java
@@ -0,0 +1,18 @@
+package z.etc;
+
+public class dandan01 {
+
+	public static void main(String[] args) {
+		int a = 2, b = 1;
+		while (a < 10 && b < 10) {
+			if(b==1)System.out.println("<"+a+"´Ü>");
+			System.out.println( a + "X" + b + "=" + a*b);
+			b++;
+			if(b==10) {
+				a++; b=1;
+				System.out.println();
+			}
+		}
+	}
+
+}
