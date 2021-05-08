c9be8d11df5611d9d516d0af280b72c1381aa383
hayoon020105 <psalter150@naver.com>
2021-05-08 23:26:37 +0900

    프로그래머스 소수찾기 리뷰

--- problem/src/programers/step1_primenumber.java
+++ problem/src/programers/step1_primenumber.java
@@ -8,8 +8,10 @@ public class step1_primenumber {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		int count = 0;
+		
 		first: for (int i = 2; i <= n; i++) {
 			int root = (int) Math.sqrt((double) i);
+			
 			for (int j = 2; j < root + 1; j++) {
 				if (i % j == 0) {
 					continue first;
