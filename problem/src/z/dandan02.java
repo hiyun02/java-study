62c947ea62cf72696bba705247d1f7e5b57c8ae6
hayoon020105 <psalter150@naver.com>
2021-05-14 23:56:40 +0900

    반복문 아껴쓰기

---
+++ problem/src/z/dandan02.java
@@ -0,0 +1,20 @@
+package z;
+
+public class dandan02 {
+
+	public static void main(String[] args) {
+		int a=2, b=1;
+		for(int i=1;i<73;i++) {
+			if(b==1) System.out.println("<"+a+"´Ü>");
+			System.out.println(a+"X"+b+"="+a*b);
+			b++;
+			if(b==10) {
+				a++; b=1;
+				System.out.println();
+			}
+		}
+		
+		
+	}
+
+}
