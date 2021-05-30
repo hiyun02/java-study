51271d520f3592ac1666c90c2fe075f20ff93163
hayoon020105 <psalter150@naver.com>
2021-05-30 17:29:34 +0900

    프로그래머스 소수만들기 정답!!

--- problem/src/programers/step1_makingprimenumber.java
+++ problem/src/programers/step1_makingprimenumber.java
@@ -4,30 +4,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class step1_makingprimenumber {
-
 	public static void main(String[] args) {
-		int[] numbers = {1, 2, 3, 4};
-		System.out.println(solution(numbers));
-
+		int arr[] = { 1, 3, 5, 7 };
+		System.out.println(solution(arr));
 	}
+
 	public static int solution(int[] nums) {
-		int answer=0;
+		int answer = 0;
+
 		ArrayList<Integer> list = new ArrayList<>();
-		for(int i : nums) {
-			for(int j : nums) {
-				for(int k : nums) {
-					if(i!=j&&j!=k&&k!=i)
-					list.add(i+j+k);
+
+		if (nums.length == 3) {
+			list.add(nums[0] + nums[1] + nums[2]);
+		} else {
+			for (int i = 0; i < nums.length; i++) {
+				for (int j = i + 1; j < nums.length; j++) {
+					for (int k = j + 1; k < nums.length; k++) {
+						if ((nums[i] + nums[j] + nums[k]) % 2 != 0)
+							list.add(nums[i] + nums[j] + nums[k]);
+
+					}
 				}
 			}
 		}
-		Collections.sort(list);
-		for(int i : list) {
-			for(int k=2; k<(int)Math.sqrt(i)+1; k++) {
-				if(i%k==0) continue;
-			}
+
+		for (int i : list) {
 			answer++;
+			for (int m = 2; m < (int) Math.sqrt(i) + 1; m++) {
+				if (i % m == 0) {
+					answer--;
+					break;
+				}
+			}
 		}
+
 		return answer;
 	}
+
 }
