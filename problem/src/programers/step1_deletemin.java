c137127e64635c4c217ce9f2315d7edf20715a1c
hayoon020105 <psalter150@naver.com>
2021-06-04 00:00:53 +0900

    프로그래머스 최솟값 제거하기

--- problem/src/programers/step1_deletemin.java
+++ problem/src/programers/step1_deletemin.java
@@ -23,6 +23,7 @@ public class step1_deletemin {
 			int min = list.get(0);
 			for(int i=0; i<list.size(); i++) {
 				if(list.get(i)<min) min = list.get(i);
+		
 			}
 			list.remove(list.indexOf(min));
 			
