8021cd766c5992ec6aa324b882cf87c2e70d4560
hayoon020105 <psalter150@naver.com>
2021-07-30 15:21:55 +0900

    백준 1009번 분산처리

---
+++ src/baekjoon/q01009.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class q01009 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			String[] in = br.readLine().split(" ");
+			int a =Integer.parseInt(in[0]);
+			int b =Integer.parseInt(in[1]);
+			
+			int answer=1;
+			for(int j=0;j<b;j++) {
+				answer*=a;
+				if(answer>9) answer%=10;
+				
+			}
+			bw.write(Integer.toString((answer==0)?0:answer));
+			bw.write("\n");
+		}
+		bw.close();
+		br.close();
+	}
+}
