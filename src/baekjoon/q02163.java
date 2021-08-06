2d3c08f5cb2de2e7c8b392ef301857e251b0bbcc
hayoon020105 <psalter150@naver.com>
2021-08-06 13:36:39 +0900

    백준 2163 초콜릿자르기

---
+++ src/baekjoon/q02163.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class q02163 {
+
+	public static int solution(int n, int m) {
+		return n*m-1;
+	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		br.close();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		st=null;
+		bw.write(Integer.toString(solution(N,M)));
+		bw.close();
+	}
+	
+}
