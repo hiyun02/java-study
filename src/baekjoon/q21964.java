76886f47ed20bebc6c4d210c719b2046e33808ad
hayoon020105 <psalter150@naver.com>
2021-08-31 23:35:54 +0900

    백준 21964번 마지막 5글자만 출력하기

---
+++ src/baekjoon/q21964.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q21964 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		System.out.println(str.substring(n-5, n));
+		br.close();
+	}
+}
