55aaaf2bdde88ea904282a36ce59bfabc37d19ba
hayoon020105 <psalter150@naver.com>
2021-09-06 23:26:18 +0900

    백준 2869 시간초과..

---
+++ src/baekjoon/q02869.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q02869 {
+
+	public static int solution(int A, int B, int V) {
+		int distance = A;
+		int day=1;
+		while(distance<V) {
+			distance-=B;
+			day++;
+			distance+=A;
+		}
+		return day;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int[] arr = new int[3];
+		for(int i=0;i<3;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		System.out.println(solution(arr[0],arr[1],arr[2]));
+	}
+
+}
