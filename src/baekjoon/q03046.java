dc338254cd4d484f2cf8f11912086dbdb8df25c4
hayoon020105 <psalter150@naver.com>
2021-09-09 23:57:08 +0900

    백준 3046 R2

---
+++ src/baekjoon/q03046.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class q03046 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a= s.nextInt();
+		int b= s.nextInt();
+		System.out.println(b+(b-a));
+	}
+
+}
